59759c6224096fa2b8e640180ebea3fc82546ded
지민 <wlals822@naver.com>
2017-01-04 22:21:08 +0900

    jump and move player1

---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Controller : MonoBehaviour {
+
+	CharacterController controller;
+
+	Vector3 moveDirection;
+
+	public float gravity;
+	public float speedJump;
+	public float speedX;
+
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetAxis ("Horizontal")>0.0f) {
+			moveDirection.x = Input.GetAxis ("Horizontal") * speedX;
+		}
+
+		if (Input.GetAxis ("Horizontal")<0.0f) {
+			moveDirection.x = Input.GetAxis ("Horizontal") * speedX;
+		}
+
+		if (controller.isGrounded) {
+			if(Input.GetButton("Jump"))
+			{
+				moveDirection.y = speedJump;
+
+			}
+		}
+
+
+		moveDirection.y -= gravity * Time.deltaTime;
+
+		Vector3 globalDirection = transform.TransformDirection (moveDirection);
+		controller.Move (globalDirection * Time.deltaTime);
+	}
+}
